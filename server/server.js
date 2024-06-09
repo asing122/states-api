8dfa18c9ca20f60433f9386b3f12a9d0ca8b317b
asing122 <annika_singh@brown.edu>
2024-06-09 23:52:34 +0800

    feat: completed challenge

---
+++ server/server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const port = 3232;
+
+const cors = require("cors");
+app.use(cors());
+
+const states = [
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming",
+];
+
+app.get("", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: states,
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server running at " + port);
+});
